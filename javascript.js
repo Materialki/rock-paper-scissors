a1b1c0178ed0888a818c376cfb64e0b1df69d4e8
Materi <mat3rialki@gmail.com>
2022-12-21 17:16:19 +0200

    Add 5 game limit

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+//html event listeners
 const rockButton = document.querySelector('#rock');
 const scissorsButton = document.querySelector("#scissors");
 const paperButton = document.querySelector("#paper");
@@ -5,13 +6,25 @@ const resultMessage = document.querySelector(".current-round-results");
 const compScoreDiv = document.querySelector("#computer-score");
 const playerScoreDiv = document.querySelector("#player-score");
 const resetButton = document.querySelector("#reset");
+const disResultsDiv = document.querySelector(".display-results")
+//
 
+//declaring choices and scores
 let playerChoice;
 let computerChoice;
 
 let computerScore = 0;
 let playerScore = 0;
+//
 
+//putting initial score in html
+compScoreDiv.textContent = computerScore;
+playerScoreDiv.textContent = playerScore;
+//
+
+let endGame = document.createElement('div');
+
+//what happens when you click the buttons
 rockButton.addEventListener('click', () => {
     playerChoice = "Rock";
     resultMessage.textContent = game();
@@ -30,7 +43,8 @@ paperButton.addEventListener('click', () => {
     playerChoice = "Paper";
     resultMessage.textContent = game();
     compScoreDiv.textContent = computerScore;
-playerScoreDiv.textContent = playerScore;
+    playerScoreDiv.textContent = playerScore;
+    isEnd();
 });
 
 resetButton.addEventListener('click', () => {
@@ -38,40 +52,58 @@ resetButton.addEventListener('click', () => {
     playerScore = 0;
     compScoreDiv.textContent = computerScore;
     playerScoreDiv.textContent = playerScore;
+    disResultsDiv.removeChild(endGame)
 })
+//
 
-compScoreDiv.textContent = computerScore;
-playerScoreDiv.textContent = playerScore;
+function isEnd() {
+
+    if (computerScore == 5 || playerScore == 5) {
+
+        if (computerScore == 5) {
+            endGame.textContent = "Sorry! The computer won. Please press the reset button to play again."
+        }
+
+        if (playerScore == 5) {
+            endGame.textContent = "Congrats! You won! Please press the reset button to play again."
+        }
+
+        disResultsDiv.appendChild(endGame)
+    }
+}
 
 function game() {
 
-    //To get a computer choice
-    let rand = Math.floor(3 * Math.random());
+    if (computerScore < 5 && playerScore < 5) {
 
-    if (rand == 0) {
-        computerChoice = "Scissors"
-    } else if (rand == 1) {
-        computerChoice = "Rock"
-    } else {
-        computerChoice = "Paper"
-    }
-    //
+        //To get a computer choice
+        let rand = Math.floor(3 * Math.random());
 
-    //Test who wins
-    function playRound() {
-        if (computerChoice == playerChoice) {
-            return `The computer chose ${computerChoice}! Draw!`
-        } else if ((computerChoice == "Rock" && playerChoice == "Scissors") || (computerChoice == "Scissors" && playerChoice == "Paper") || (computerChoice == "Paper" && playerChoice == "Rock") ) {
-            computerScore++;
-            return `The computer chose ${computerChoice}! You lose!`;
-        } else if ((playerChoice == "Rock" && computerChoice == "Scissors") || (playerChoice == "Scissors" && computerChoice == "Paper") || (playerChoice == "Paper" && computerChoice == "Rock") ) {
-            playerScore++;
-            return `The computer chose ${computerChoice}! You win!`
+        if (rand == 0) {
+            computerChoice = "Scissors"
+        } else if (rand == 1) {
+            computerChoice = "Rock"
+        } else {
+            computerChoice = "Paper"
         }
-    }
-    //
+        //
 
-    return playRound()
+        //Test who wins
+        function playRound() {
+            if (computerChoice == playerChoice) {
+                return `The computer chose ${computerChoice}! Draw!`
+            } else if ((computerChoice == "Rock" && playerChoice == "Scissors") || (computerChoice == "Scissors" && playerChoice == "Paper") || (computerChoice == "Paper" && playerChoice == "Rock") ) {
+                computerScore++;
+                return `The computer chose ${computerChoice}! You lose!`;
+            } else if ((playerChoice == "Rock" && computerChoice == "Scissors") || (playerChoice == "Scissors" && computerChoice == "Paper") || (playerChoice == "Paper" && computerChoice == "Rock") ) {
+                playerScore++;
+                return `The computer chose ${computerChoice}! You win!`
+            }
+        }
+        //
+
+        return playRound()
+    } 
 }
 
 
