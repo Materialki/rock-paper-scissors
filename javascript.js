355e90b1cb072fc6614c3ca5c6351c5da7c66257
Materi <mat3rialki@gmail.com>
2022-12-21 13:06:48 +0200

    Change player input from typing to button

--- javascript.js
+++ javascript.js
@@ -1,11 +1,35 @@
-let computerScore = 0;
-let playerScore = 0;
+const rockButton = document.querySelector('#rock');
+const scissorsButton = document.querySelector("#scissors");
+const paperButton = document.querySelector("#paper");
+
+let playerChoice;
+
+rockButton.addEventListener('click', () => {
+    playerChoice = "Rock";
+    console.log(game());
+});
+
+scissorsButton.addEventListener('click', () => {
+    playerChoice = "Scissors";
+    console.log(game());
+});
+
+paperButton.addEventListener('click', () => {
+    playerChoice = "Paper";
+    console.log(game());
+});
+
+
+// let computerScore = 0;
+// let playerScore = 0;
 let computerChoice;
 
-function printScore() {
-    return ` Your points: ${playerScore}
-    The computer\'s points: ${computerScore}`
-}
+// function printScore() {
+//     return ` Your points: ${playerScore}
+//     The computer\'s points: ${computerScore}`
+// }
+
+
 
 function game() {
 
@@ -22,29 +46,29 @@ function game() {
     //
 
 
-    //To get a player choice
-    let playerChoice = prompt("Rock, scissors, or paper?", "type here");
-    //
+    // //To get a player choice
+    // let playerChoice = prompt("Rock, scissors, or paper?", "type here");
+    // //
 
-    //To make the player choice case insensitive
-    playerChoice = playerChoice.toLowerCase();
-    let firstLetter = playerChoice.slice(0,1);
-    firstLetter = firstLetter.toUpperCase();
-    playerChoice = firstLetter + playerChoice.slice(1);
-    //
+//     //To make the player choice case insensitive
+//     playerChoice = playerChoice.toLowerCase();
+//     let firstLetter = playerChoice.slice(0,1);
+//     firstLetter = firstLetter.toUpperCase();
+//     playerChoice = firstLetter + playerChoice.slice(1);
+//     //
 
     //Test who wins
     function playRound() {
         if (computerChoice == playerChoice) {
-            return `The computer chose ${computerChoice}! Draw!` + printScore()
+            return `The computer chose ${computerChoice}! Draw!`
         } else if ((computerChoice == "Rock" && playerChoice == "Scissors") || (computerChoice == "Scissors" && playerChoice == "Paper") || (computerChoice == "Paper" && playerChoice == "Rock") ) {
-            computerScore++;
-            return `The computer chose ${computerChoice}! You lose!` + printScore()
+            // computerScore++;
+            return `The computer chose ${computerChoice}! You lose!`;
         } else if ((playerChoice == "Rock" && computerChoice == "Scissors") || (playerChoice == "Scissors" && computerChoice == "Paper") || (playerChoice == "Paper" && computerChoice == "Rock") ) {
-            playerScore++;
-            return `The computer chose ${computerChoice}! You win!` + printScore()
-        } else {
-            return "Please type rock scissors or paper."
+            // playerScore++;
+            return `The computer chose ${computerChoice}! You win!`
+        // } else {
+        //     return "Please type rock scissors or paper."
         }
     }
     //
@@ -53,8 +77,8 @@ function game() {
 }
 
 
-while (computerScore + playerScore < 5) {
-    console.log(game());
-}
+// while (computerScore + playerScore < 5) {
+    // console.log(game());
+// }
 
 
